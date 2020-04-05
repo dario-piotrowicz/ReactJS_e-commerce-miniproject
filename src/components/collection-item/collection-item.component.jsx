ba7afc536cac095cce4723467bf1199c0903d57b
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-04-05 20:28:53 +0200

    created collection-item component with styling + added collection-preview styling

---
+++ src/components/collection-item/collection-item.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './collection-item.styles.scss'
+
+const CollectionItem = ({ id, name, price, imageUrl }) => (
+    <div className="collection-item">
+        <div className="image"
+             style={{ backgroundImage: `url(${imageUrl})`}}/>
+        <div className="footer">
+            <span class="name">{name}</span>
+            <span class="price">${price}</span>
+        </div>
+    </div>
+);
+
+export default CollectionItem;
--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './collection-preview.styles.scss';
+import CollectionItem from '../collection-item/collection-item.component';
 
 const CollectionPreview = ({ title, items }) => (
  <div className="collection-preview">
     <h1 className="title">{title.toUpperCase()}</h1>
-        <div className="preview">
+        <div className="previews">
             {
                 items.filter( (_, idx) => idx < 4 )
-                     .map( item => <div key={item.id}>{item.name}</div> )
+                     .map( ({id, ...otherProps}) => 
+                        <CollectionItem key={id} {...otherProps}/> )
             }
         </div>
  </div>
