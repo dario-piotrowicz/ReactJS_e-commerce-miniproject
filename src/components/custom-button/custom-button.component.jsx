f6b1798bb18193fd78e1c90feddf5b5abc5c68da
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-04-11 14:37:14 +0200

    CustomBotton added missing use of otherProps

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './custom-button.styles.scss'
 
 const CustomButton = ({ children, ...otherProps }) => (
-    <button className="custom-button">
+    <button className="custom-button" {...otherProps}>
         {children}
     </button>
 );
