8d88b7f2482c4e793e525b0e658160ed146964d0
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-04-18 15:03:03 +0200

    customButton, added invertedColors prop and styling

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './custom-button.styles.scss'
 
-const CustomButton = ({ children, isGoogleSignIn, ...otherProps }) => (
-    <button className={ `${isGoogleSignIn ? 'google-sign-in' : '' } custom-button` } {...otherProps}>
+const CustomButton = ({ children, isGoogleSignIn, invertedColors , ...otherProps }) => {
+    let className = 'custom-button';
+    if ( isGoogleSignIn ) className += ' google-sign-in';
+    if ( invertedColors ) className += ' inverted-colors';
+    return <button className={className} {...otherProps}>
         {children}
     </button>
-);
+};
 
 export default CustomButton;
