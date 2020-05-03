9c42cd9d69549eba39953ce39b09d8f50b9d20dc
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-03 18:55:42 +0200

    removed redux-thunk as it is no longer necessary/used

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,11 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './root-reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
-import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
 import { requestShopDataUpdatesFromFirestore } from './shop/shop.sagas';
 
 const sagaMiddleware = createSagaMiddleware();
-const middlewares = [ thunk, sagaMiddleware ];
+const middlewares = [ sagaMiddleware ];
 
 export const store = createStore( rootReducer,
                            composeWithDevTools(
