843372aadc15b9fb6dbdf5b6c75bb2b2a62788d1
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-09 15:12:37 +0200

    added actual stripe payment processing method

--- src/components/stripe-checkout-button/stripe-checkout-button.component.jsx
+++ src/components/stripe-checkout-button/stripe-checkout-button.component.jsx
@@ -3,14 +3,44 @@ import './stripe-checkout-button.styles.scss'
 import StripeCheckout from 'react-stripe-checkout';
 import { clearAllItems } from '../../redux/cart/cart.actions';
 import { connect } from 'react-redux';
+import axios from 'axios';
+
+const stripeCurrency = 'USD';
 
 const StripeCheckoutButton = ({ price, clearCartItems }) => {
     const priceInCents =  price * 100;
     const stripePublishablkey = 'pk_test_1zuA92hpdJl6iYUrOeNkKckB005z3T4ZvH';
 
-    const onTokenHandler = token => {
-        alert('Payment Successful');
-        clearCartItems();
+    const onTokenHandler = async token => {
+        try {
+            await axios({
+                url: 'stripe-payment',
+                method: 'post',
+                data: {
+                    amount: priceInCents,
+                    currency: stripeCurrency,
+                    token
+                }
+            });
+
+            alert('Payment Successful!');
+            clearCartItems();
+        } catch( error ){
+            if(error.response.status === 404 ){
+                alert('Error: Backend not found');
+            } else {
+                const stripeErrorData = error.response.data;
+                const stripeErrorCode = stripeErrorData ? stripeErrorData.code : null;
+                switch(stripeErrorCode){
+                    case 'card_declined':
+                        alert('Error: Wrong card provided, please use the test card provided');
+                        break;
+                    default:
+                        alert('Error: Payment aborted');
+                        break;
+                }
+            }
+        }
     }
 
     return <div className="stipe-checkout-button">
@@ -25,7 +55,7 @@ const StripeCheckoutButton = ({ price, clearCartItems }) => {
                 (Expiration can be any future date and CVV can be any 3 digit number)
             </p>
         </div>
-        <StripeCheckout 
+        <StripeCheckout
                 label='Pay Now'
                 name='E-Commerce Miniproject'
                 billingAddress
@@ -36,6 +66,7 @@ const StripeCheckoutButton = ({ price, clearCartItems }) => {
                 panelLabel='Pay Now'
                 token={onTokenHandler}
                 stripeKey={stripePublishablkey}
+                currency={stripeCurrency}
                 disabled={ price === 0 }
                 />
     </div>;
