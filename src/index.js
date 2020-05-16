c338995a75edab3422fc8af7a8679614bdc97bd5
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-05-16 20:03:39 +0200

    made application PWA compliant

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -19,3 +20,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorker.register();
