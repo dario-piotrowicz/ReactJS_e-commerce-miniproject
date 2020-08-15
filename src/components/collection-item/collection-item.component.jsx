6b0c9847c77fcf799b78abc19f1d962dbce84a9d
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-08-15 15:55:48 +0200

    add to cart disabled if user isn't logged in

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './collection-item.styles.scss'
 import CustomButton from '../custom-button/custom-button.component';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addItem } from '../../redux/cart/cart.actions';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { toast } from 'react-toastify';
 
 const CollectionItem = ({ item }) => {
     const { name, price, imageUrl } = item;
 
+    const [addButtonDisabled, setAddButtonDisabled] = useState(false);
+
+    const currentUser = useSelector(selectCurrentUser);
     const dispatch = useDispatch();
+
+    const addItemToCart = () => {
+        if(!currentUser){
+            toast.error('Log in to add an item to your cart');
+            setAddButtonDisabled(true);
+            setTimeout( () => {
+                setAddButtonDisabled(false);
+            },3000);
+        } else {
+            dispatch(addItem(item));
+        }
+    };
 
     return <div className="collection-item">
         <div className="image"
@@ -16,7 +33,7 @@ const CollectionItem = ({ item }) => {
             <span className="name">{name}</span>
             <span className="price">${price}</span>
         </div>
-        <CustomButton onClick={() => dispatch(addItem(item))} invertedColors>
+        <CustomButton onClick={addItemToCart} invertedColors disabled={ addButtonDisabled ? true : false}>
             Add To Cart
         </CustomButton>
     </div>
