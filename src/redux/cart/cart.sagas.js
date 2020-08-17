397f480f2f541675764936920ef3aa9e3d9da813
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-08-17 21:20:25 +0200

    cleaned cart sagas and reduced code duplication

--- src/redux/cart/cart.sagas.js
+++ src/redux/cart/cart.sagas.js
@@ -1,85 +1,69 @@
-import { take, takeEvery, call, put, all, select } from 'redux-saga/effects';
+import { take, call, put, all, select } from 'redux-saga/effects';
 import { firestoreUtils } from '../../firebase/firebase.utils';
 import { addItemLocally, removeItemLocally, clearItemLocally, clearAllItemsLocally } from './cart.actions';
 import cartActionTypes from './cart.types';
 import { selectCurrentUser } from '../user/user.selectors';
 import { toast } from 'react-toastify';
 
-
-function* handleAddItemAction(item){
+function* handleDbAction(firestoreUtiliFunction, itemId, errorMessage, actionToEmitOnSuccess){
   const currentUser = yield select(selectCurrentUser);
 
   try {
-    yield firestoreUtils.addItemToDbCart(currentUser.id, item.id);
+    yield firestoreUtiliFunction(currentUser.id, itemId);
   }catch(error){
-    toast.error('A network error has occurred and your item cannot be added to the cart at this moment. Please try again later');
-    return;
-  }
-  
-  yield put((addItemLocally(item)));
-}
-
-
-function* handleRemoveItemAction(item){
-  const currentUser = yield select(selectCurrentUser);
-
-  try {
-    yield firestoreUtils.removeItemFromDbCart(currentUser.id, item.id);
-  }catch(error){
-    toast.error('An error has occurred and your item cannot be removed from the cart at this moment. Please try again later');
+    toast.error(errorMessage);
     return;
   }
 
-  yield put((removeItemLocally(item)));
-}
-
-
-function* handleClearItemAction({ payload: item }){
-  const currentUser = yield select(selectCurrentUser);
-
-  try {
-    yield firestoreUtils.clearItemFromDbCart(currentUser.id, item.id);
-  }catch(error){
-    toast.error('An error has occurred and your item cannot be removed from the cart at this moment. Please try again later');
-    return;
-  }
-
-  yield put((clearItemLocally(item)));
-}
-
-function* handleClearAllItemsAction(){
-  const currentUser = yield select(selectCurrentUser);
-
-  try {
-    yield firestoreUtils.clearAllItemsFromDbCart(currentUser.id);
-  }catch(error){
-    toast.error('An error has occurred and your cart could not be cleared at this moment. Please try again later');
-    return;
-  }
-
-  yield put((clearAllItemsLocally()));
+  yield put(actionToEmitOnSuccess);
 }
 
 export function* addItem(){
   while( true ){
-    const { payload } = yield take(cartActionTypes.ADD_ITEM);
-    yield handleAddItemAction(payload);
+    const { payload: item } = yield take(cartActionTypes.ADD_ITEM);
+    yield handleDbAction(
+      firestoreUtils.addItemToDbCart,
+      item.id,
+      'A network error has occurred and your item cannot be added to the cart at this moment. Please try again later',
+      addItemLocally(item)
+    );
   }
 }
 
 export function* removeItem(){
   while( true ){
-   const { payload } = yield take(cartActionTypes.REMOVE_ITEM);
-   yield handleRemoveItemAction(payload);
+   const { payload: item } = yield take(cartActionTypes.REMOVE_ITEM);
+   yield handleDbAction(
+      firestoreUtils.removeItemFromDbCart,
+      item.id,
+      'An error has occurred and your item cannot be removed from the cart at this moment. Please try again later',
+      removeItemLocally(item)
+    );
   }
 }
 
 export function* clearItem(){
-  yield takeEvery(cartActionTypes.CLEAR_ITEM, handleClearItemAction);
+  while( true ){
+    const { payload: item } = yield take(cartActionTypes.CLEAR_ITEM);
+    yield handleDbAction(
+      firestoreUtils.clearItemFromDbCart,
+      item.id,
+      'An error has occurred and your item cannot be removed from the cart at this moment. Please try again later',
+      clearItemLocally(item)
+    );
+  }
 }
 
 export function* clearAllItems(){
-  yield takeEvery(cartActionTypes.CLEAR_ALL_ITEMS, handleClearAllItemsAction);
+  while( true ){
+    const { payload: item } = yield take(cartActionTypes.CLEAR_ALL_ITEMS);
+    yield handleDbAction(
+      firestoreUtils.clearAllItemsFromDbCart,
+      item.id,
+      'An error has occurred and your cart could not be cleared at this moment. Please try again later',
+      clearAllItemsLocally()
+    );
+  }
 }
 
 export default function* cartSagas(){
